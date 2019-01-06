54860e8393f6cd82cb1aa45194352ec1a2ae4cbc
chitty <rychitty@icloud.com>
2019-01-05 21:57:38 -0600

    pushed

---
+++ bamazon.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE Bamazon_db;
+
+USE Bamazon_db;
+
+CREATE TABLE products(
+	id INT NOT NULL AUTO_INCREMENT,
+	ProductName VARCHAR(100) NOT NULL,
+	DepartmentName VARCHAR(100) NOT NULL,
+	Price DECIMAL(10,2) default 0,
+	StockQuantity INT default 0,
+	PRIMARY KEY(id)
+);
+
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Brooks Launch 2', 'Running Shoes', 99, 12);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Adidas Supernova Glide 8', 'Running Shoes', 129.95, 20);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Mizuno Wave Rider 18', 'Running Shoes', 119.95, 10);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Nike LUNARGLIDE 8', 'Running Shoes', 119.95, 33);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Nike Men\'s Running Tee', 'Apparel', 70, 10);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Nike Men\'s Running Tank', 'Apparel', 45, 12);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Nike Women\'s Running Tee', 'Apparel', 75, 40);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Nike Women\'s Running Tank', 'Apparel', 50, 3);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Hydration Belt', 'Accessories', 55, 4);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Headlamp', 'Accessories', 25, 20);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('GPS Watch', 'Electronics', 299.99, 2);
+
+CREATE TABLE departments (
+	DepartmentId INT NOT NULL AUTO_INCREMENT,
+	DepartmentName VARCHAR(100) NOT NULL,
+	OverheadCost DECIMAL(10,2) NOT NULL,
+	TotalSales DECIMAL(10,2),
+	PRIMARY KEY(DepartmentId)
+);
+
+INSERT INTO departments(DepartmentName, OverheadCost) VALUES('Running Shoes', 500);
+INSERT INTO departments(DepartmentName, OverheadCost) VALUES('Apparel', 500);
+INSERT INTO departments(DepartmentName, OverheadCost) VALUES('Electronics', 500);
+INSERT INTO departments(DepartmentName, OverheadCost) VALUES('Accessories', 500);
